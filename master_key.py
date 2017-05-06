5016882372960d9b70cdf0dbba120a340d98c92d
noisyfox <timemanager.rick@gmail.com>
2017-05-07 02:03:42 +1000

    Add code for key generating

---
+++ master_key.py
@@ -0,0 +1,18 @@
+# for generating new key pairs
+from Crypto.PublicKey import RSA
+
+if __name__ == '__main__':
+    i = input("This will overwrite any existing keys! Proceed? (y/N):")
+    if i.lower() == 'y':
+        print('Generating RSA key...')
+        key = RSA.generate(bits=2048)
+
+        print('Saving private key...')
+        with open('master_key.pem', 'wb') as f:
+            f.write(key.exportKey('PEM'))
+
+        print('Saving public key...')
+        with open('./lib/key_rsa.pub.pem', 'wb') as f:
+            f.write(key.publickey().exportKey('PEM'))
+
+        print('All done!')
